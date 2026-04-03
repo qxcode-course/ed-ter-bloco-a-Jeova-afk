57012e6be2faba681da19e2f55d5542ae32de6eb
Jeova-afk <jeovaalison00@gmail.com>
2026-04-03 17:13:48 -0300

    triangulo ed

---
+++ ed/triangulo/go/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func processa(vet []int) {
+
+	if len(vet) == 1 {
+		fmt.Println("[", Join(vet, " "),  "]")
+		return
+	}
+
+	auxiliar := []int{}
+	for i := 0; i < len(vet)-1; i++{
+		auxiliar = append(auxiliar, vet[i]+vet[i+1])
+
+	} 
+
+	processa(auxiliar)
+	
+	fmt.Println("[", Join(vet, " "), "]")
+	
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		return
+	}
+	line := scanner.Text()
+	parts := strings.Fields(line)
+	vet := []int{}
+	for _, part := range parts {
+		if value, err := strconv.Atoi(part); err == nil {
+			vet = append(vet, value)
+		}
+	}
+	processa(vet)
+}
+
+func Join[T any](v []T, sep string) string {
+	if len(v) == 0 {
+		return ""
+	}
+	s := ""
+	for i, x := range v {
+		if i > 0 {
+			s += sep
+		}
+		s += fmt.Sprintf("%v", x)
+	}
+	return s
+}
